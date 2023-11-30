92f1c2b81085f00d3267ca4ba7ca3ea7159072c1
aeioou <150875552+aeioou@users.noreply.github.com>
2023-11-30 21:13:00 +0900

    Add files via upload

---
+++ dfsfdd.m
@@ -0,0 +1,9 @@
+lat34=Position34(:,1); lon34=Position34(:,2);
+lat54=Position54(:,1); lon54=Position54(:,2);
+lat64=Position64(:,1); lon64=Position64(:,2);
+lat34=table2array(lat34); lon34=table2array(lon34);
+lat54=table2array(lat54); lon54=table2array(lon54);
+lat64=table2array(lat64); lon64=table2array(lon64);
+l=distance(lat64, lon64);
+geoplot(lat64,lon64);
+fprintf('거리는 %.1f미터\n',l);
